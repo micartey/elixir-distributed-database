b2fbe67865bf84956e37cdf29efa875cca190fdd
micartey <me@micartey.dev>
2024-12-08 18:05:19 +0100

    implement auth method

--- lib/router/Router.ex
+++ lib/router/Router.ex
@@ -27,49 +27,24 @@ defmodule Router.Router do
     {:ok, body_raw, _conn} = Plug.Conn.read_body(conn)
     body = Poison.Parser.parse!(body_raw, %{keys: :atoms!})
 
-    case User.UserServer.auth_user?(body[:username], body[:password]) do
-      true ->
+    case User.UserServer.auth_user(body[:username], body[:password]) do
+      nil ->
+        send_resp(conn, 401, Poison.encode!(%{error: "User not found or incorrect password"}))
+
+      user ->
         {:ok, token, _plain} =
           JwtConfig.generate_token(%{
             "sub" =>
-              :crypto.hash(:sha, body[:username])
+              :crypto.hash(:sha, user.username)
               |> :crypto.bytes_to_integer()
               |> Integer.digits()
               |> Enum.map(fn digit -> <<digit + 100::utf8>> end)
-              |> Enum.join("")
+              |> Enum.join(""),
+            "permission" => user.permission
           })
+
+        send_resp(conn, 200, Poison.encode!(%{token: token}))
     end
-
-    # TODO: Search user on all nodes. If they know the user, make sure all of them have the same value
-    # If not, there is an error - log it
-    # Check what the local storage says - we don't trust the others (maybe imposters?)
-    # If we don't know it, abort: Something wen't wrong (Generate trace id)
-
-    # Retrive User from Database based on email and password
-    # case Repo.get_by(User, [email: body[:email], password: body[:password]]) do
-    #   # Email-Password combo not found
-    #   nil -> send_resp(conn, 401, Poison.encode!(%{error: "User not found or incorrect password"}))
-
-    #   # User found - Create JWT-Token
-    #   user ->
-    #     user = Repo.preload(user, [:permissions])
-    #     user_permission = Repo.preload(user.permissions, :permission)
-
-    #     {:ok, token, _plain} = JwtConfig.generate_token(%{
-    #       "sub" => user.id,
-    #       "uuid" => :crypto.hash(:sha, user.name)
-    #       |> :crypto.bytes_to_integer()
-    #       |> Integer.digits()
-    #       |> Enum.map(fn digit -> <<(digit + 100)::utf8>> end)
-    #       |> Enum.join(""),
-
-    #       "permissions" => user_permission |> Enum.map(fn entry -> entry.permission.name end) |> Enum.join(", ")
-    #     })
-
-    #     send_resp(conn, 200, Poison.encode!(%{token: token}))
-    # end
-
-    send_resp(conn, 401, Poison.encode!(%{error: "User not found or incorrect password"}))
   end
 
   match _ do
