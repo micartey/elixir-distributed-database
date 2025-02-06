2095294bed20724c2ae9979344a6b5535b655482
micartey <me@micartey.dev>
2025-02-06 22:13:33 +0100

    feat: add put method

--- lib/router/Router.ex
+++ lib/router/Router.ex
@@ -65,6 +65,46 @@ defmodule Router.Router do
     send_resp(conn, 200, Poison.encode!(result))
   end
 
+  @doc """
+  Put a key-value into a topic.
+
+  The body of the request should be a JSON object with the following structure:
+
+  {
+    "topic": "...",
+    "key": "...",
+    "value": "...",
+    "old_value": "..." (optional)
+  }
+
+  If old_value is present, the put operation will use optimistic locking.
+  """
+  put "/put" do
+    {:ok, body_raw, _conn} = Plug.Conn.read_body(conn)
+
+    body = Poison.Parser.parse!(body_raw, %{keys: :atoms!})
+
+    # Use optimistic locking if old_data is present
+    # Otherwise, just put the data into the database
+    result =
+      if Map.has_key?(body, :old_value) do
+        Database.Database.get_worker(body[:topic])
+        |> GenServer.call({:put, body[:topic], body[:key], body[:old_value], body[:value]})
+      else
+        Database.Database.get_worker(body[:topic])
+        |> GenServer.call({:put, body[:topic], body[:key], body[:value]})
+      end
+
+    # Check if the result is a failure
+    case result do
+      :fail ->
+        send_resp(conn, 409, Poison.encode!(%{error: "Conflict"}))
+
+      _ ->
+        send_resp(conn, 200, Poison.encode!(result))
+    end
+  end
+
   match _ do
     send_resp(conn, 404, "oops")
   end
