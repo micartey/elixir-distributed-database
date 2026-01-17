0cfca781f1f8474a0ca2069b53cc60d2e1af799d
micartey <me@micartey.dev>
2026-01-17 20:45:57 +0100

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Eddb.MixProject do
   def project do
     [
       app: :eddb,
-      version: "0.9.2",
+      version: "0.10.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       description: description(),
